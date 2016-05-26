71dba68feae305a3417fc274447a91e7c2b2a0e4
Tobi <tobi.bechtold@gmail.com>
2016-05-27 01:15:32 +0200

    added blog header component

---
+++ app/header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+  render() {
+    return <div className="blog-header">
+      <h1>Tobi's Blog</h1>
+    </div>
+  }
+}
